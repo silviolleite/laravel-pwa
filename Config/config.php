94967b9682ee40d52abc80812a508fe65cc931d3
Michael Umfahrer <michael@umfahrer.dev>
2020-06-13 16:16:39 +0200

    Add shortcuts functionality for PWAs

--- Config/config.php
+++ Config/config.php
@@ -57,6 +57,22 @@ return [
             '1668x2388' => '/images/icons/splash-1668x2388.png',
             '2048x2732' => '/images/icons/splash-2048x2732.png',
         ],
+        'shortcuts' => [
+            [
+                'name' => 'Shortcut Link 1',
+                'description' => 'Shortcut Link 1 Description',
+                'url' => '/shortcutlink1',
+                'icons' => [
+                    "src" => "/images/icons/icon-72x72.png",
+                    "purpose" => "any"
+                ]
+            ],
+            [
+                'name' => 'Shortcut Link 2',
+                'description' => 'Shortcut Link 2 Description',
+                'url' => '/shortcutlink2'
+            ]
+        ],
         'custom' => []
     ]
 ];
