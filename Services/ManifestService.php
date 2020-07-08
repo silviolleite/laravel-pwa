9acc2efdc7882312b3a8e80daf7582640b85b5d4
Michael Umfahrer <michael@umfahrer.dev>
2020-07-08 10:18:18 +0200

    provide backward compatibility for people who do not have a shortcut section in their config

--- Services/ManifestService.php
+++ Services/ManifestService.php
@@ -35,27 +35,29 @@ class ManifestService
             ];
         }
 
-        foreach (config('laravelpwa.manifest.shortcuts') as $shortcut) {
+        if (config('laravelpwa.manifest.shortcuts')) {
+            foreach (config('laravelpwa.manifest.shortcuts') as $shortcut) {
 
-            if (array_key_exists("icons", $shortcut)) {
-                $fileInfo = pathinfo($shortcut['icons']['src']);
-                $icon = [
-                    'src' => $shortcut['icons']['src'],
-                    'type' => 'image/' . $fileInfo['extension'],
-                    'purpose' => $shortcut['icons']['purpose']
-                ];
-            } else {
-                $icon = [];
-            }
+                if (array_key_exists("icons", $shortcut)) {
+                    $fileInfo = pathinfo($shortcut['icons']['src']);
+                    $icon = [
+                        'src' => $shortcut['icons']['src'],
+                        'type' => 'image/' . $fileInfo['extension'],
+                        'purpose' => $shortcut['icons']['purpose']
+                    ];
+                } else {
+                    $icon = [];
+                }
 
-            $basicManifest['shortcuts'][] = [
-                'name' => trans($shortcut['name']),
-                'description' => trans($shortcut['description']),
-                'url' => $shortcut['url'],
-                'icons' => [
+                $basicManifest['shortcuts'][] = [
+                    'name' => trans($shortcut['name']),
+                    'description' => trans($shortcut['description']),
+                    'url' => $shortcut['url'],
+                    'icons' => [
                         $icon
                     ]
-            ];
+                ];
+            }
         }
 
         foreach (config('laravelpwa.manifest.custom') as $tag => $value) {
